97e5cc3cc9e4a3420e5aad7bb9cc2cc9ca2a9c7f
Darren Liew <ldarren@gmail.com>
2017-10-08 04:03:49 +0000

    added tutorial

--- js/main.js
+++ js/main.js
@@ -72,12 +72,37 @@ E(document).ready(function () {
     // LOCAL STORAGE
     // ------------------------------
     if (localStorage.getItem('jsoncode') === null) {
-        var defaultJSON = ```
-		{
-			"topic": {
+        var defaultJSON = `{
+			"tutorials": {
+				"list": [
+					{
+						"topic": "Crawl before you walk",
+						"hint": "at lower pane (console) type $('tutorials') to go to child tutorial node",
+						"todo": "go to list child node"
+					},{
+						"topic": "Indexing",
+						"hint": "at console type $('tutorials')('list')(0, 1) to see this tutorial 0 and 1 only, JWalk index is 0 based",
+						"todo": "see tutorial 0, 1 and 2"
+					},{
+						"topic": "Listing",
+						"hint": "at console type $('tutorials')('list')([0, -1, 1]) to list all tutorials at step size of 1",
+						"todo": "list odd index tutorials only"
+					},{
+						"topic": "Fast Forward",
+						"hint": "at console type $('..', 'topic') to fast forward to all tutorial topic",
+						"todo": "fast forward to all todo"
+					},{
+						"topic": "Be Wild",
+						"hint": "at console type $('tutorials')( '*') to see all tutorials children",
+						"todo": "see all list children"
+					},{
+						"topic": "Filtering",
+						"hint": "at console type $('..','list')('*')( tutorial => tutorial.topic === 'Filtering') to this tutorial only",
+						"todo": "See tutorial with topic equal Listing"
+					}
+				]
 			}
-		}
-		```;
+		}`;
         localStorage.setItem('jsoncode', defaultJSON);
     }
     
